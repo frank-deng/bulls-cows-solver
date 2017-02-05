9a99c4171a340da56081d05a9f36677f6d509185
frank-deng <13382084679@163.com>
2017-02-05 16:35:33 +0800

    Add feature of detecting invalid input, as well as test cases needed for this. Add web-based example.

--- bulls-cows-solver.js
+++ bulls-cows-solver.js
@@ -519,6 +519,27 @@
 '9863','9864','9865','9867','9870','9871','9872','9873','9874','9875',
 '9876',
     ];
+	var isValidInput = function(guess, result) {
+		var validResults = [
+			'0A0B', '0A1B', '0A2B', '0A3B', '0A4B',
+			'1A0B', '1A1B', '1A2B', '1A3B',
+			'2A0B', '2A1B', '2A2B', '3A0B',
+			'4A0B',
+		];
+		if (!/^\d{4}$/.test(guess)) {
+			return false;
+		}
+		for (var i = 0; i < 3; i++) {
+			var c = guess.charAt(i);
+			if (guess.indexOf(c) != guess.lastIndexOf(c)) {
+				return false;
+			}
+		}
+		if (-1 == validResults.indexOf(result)) {
+			return false;
+		}
+		return true;
+	}
     var compare = function(n0,n1){
 		var i, j, a=0, b=0;
 		for (i = 0; i < 4; i++) {
@@ -535,6 +556,25 @@
 		return a+'A'+b+'B';
 	}
     return function(filter){
+		if (!(filter instanceof Array)) {
+			throw 'Invalid input.';
+		}
+
+		if (filter.length == 0) {
+			return undefined;
+		}
+
+		for (var i = 0; i < filter.length; i++) {
+			var f = filter[i];
+			try {
+				if (!isValidInput(f.guess, f.result)){
+					throw 'Input '+(i+1)+' is invalid.';
+				}
+			} catch(e) {
+				throw 'Input '+(i+1)+' is invalid.';
+			}
+		}
+
 		var result = new Array(), filtered = false, candidate;
 		for (var i = 0; i < candidates_all.length; i++) {
 			candidate = candidates_all[i];
--- test.js
+++ test.js
@@ -6,4 +6,88 @@ QUnit.test("Test", function(assert) {
 		]),
 		[ '1342', '1423', '2314', '2431', '3124', '3241', '4132', '4213' ]
 	);
+	assert.strictEqual(bulls_cows_solver([]), undefined);
+});
+QUnit.test("Invalid Input Detection", function(assert) {
+	assert.throws(function(){bulls_cows_solver();}, /Invalid input./, "Detect invalid input.");
+	assert.throws(function(){bulls_cows_solver("1324");}, /Invalid input./, "Detect invalid input.");
+	assert.throws(function(){bulls_cows_solver(23432);}, /Invalid input./, "Detect invalid input.");
+	assert.throws(
+		function(){
+			bulls_cows_solver([
+				{guess:'1234', result:'1A3B'},
+				{guess:'32a9',},
+				{guess:'3234', result:'1A3B'},
+			]);
+		},
+		/Input 2 is invalid./,
+		"Detect abnormal record."
+	);
+	assert.throws(
+		function(){
+			bulls_cows_solver([
+				{guess:'1234', result:'1A3B'},
+				{guess:'32a9', result:'0A0B'},
+				{guess:'3234', result:'1A3B'},
+			]);
+		},
+		/Input 2 is invalid./,
+		"Detect non-number input."
+	);
+	assert.throws(
+		function(){
+			bulls_cows_solver([
+				{guess:'1234', result:'1A3B'},
+				{guess:'678', result:'0A0B'},
+				{guess:'3234', result:'1A3B'},
+			]);
+		},
+		/Input 2 is invalid./,
+		"Detect number shorter than 4."
+	);
+	assert.throws(
+		function(){
+			bulls_cows_solver([
+				{guess:'1234', result:'1A3B'},
+				{guess:'67890', result:'0A0B'},
+				{guess:'3234', result:'1A3B'},
+			]);
+		},
+		/Input 2 is invalid./,
+		"Detect number longer than 4."
+	);
+	assert.throws(
+		function(){
+			bulls_cows_solver([
+				{guess:'1234', result:'1A3B'},
+				{guess:'5678', result:'0A0B'},
+				{guess:'3234', result:'1A3B'},
+				{guess:'1234', result:'1A3B'},
+			]);
+		},
+		/Input 3 is invalid./,
+		"Number with duplicated digit."
+	);
+	assert.throws(
+		function(){
+			bulls_cows_solver([
+				{guess:'1234', result:'1A3B'},
+				{guess:'1234', result:'1A4B'},
+				{guess:'1234', result:'2A1B'},
+			]);
+		},
+		/Input 2 is invalid./,
+		"Invalid guess result."
+	);
+	assert.throws(
+		function(){
+			bulls_cows_solver([
+				{guess:'1234', result:'1A3B'},
+				{guess:'4234', result:'1A4B'},
+				{guess:'1234', result:'2A1B'},
+			]);
+		},
+		/Input 2 is invalid./,
+		"Invalid number and guess result."
+	);
 });
