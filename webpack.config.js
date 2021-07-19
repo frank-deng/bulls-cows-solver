f332b2ce845c2728ebdf2558403f61f880ede1e2
frank-deng <13382084679@163.com>
2021-07-19 18:13:00 +0800

    Testing page merge style, js into index.html

--- webpack.config.js
+++ webpack.config.js
@@ -1,17 +1,25 @@
 const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 module.exports = {
   mode: 'production',
   entry: {
-    main:[
-      './src/bulls-cows-solver.js'
-    ]
+    main:{
+      import:'./src/bulls-cows-solver.js',
+      library:{
+        name: 'bullsCowsSolver',
+        export: 'default',
+        type: 'umd',
+      }
+    },
+    testpage:{
+      import:'./src/index.js'
+    }
   },
-  output: {
-    path: path.resolve(__dirname,'dist'),
-    filename: 'bulls-cows-solver.js',
-    library:'bullsCowsSolver',
-    libraryExport:'default',
-    libraryTarget:'umd'
+  output:{
+    path: __dirname,
+    filename(data){
+      return `${data.runtime}.js`;
+    }
   },
   module: {
     rules: [
@@ -25,6 +33,37 @@ module.exports = {
         },
         exclude: /node_modules/
       },
+      {
+        test: /\.less$/,
+        use: [
+          {
+            loader:"style-loader"
+          },
+          {
+            loader:"css-loader"
+          },
+          {
+            loader:"postcss-loader",
+            options: {
+              postcssOptions:{
+                plugins: {
+                  'cssnano': {}
+                }
+              }
+            }
+          },
+          {
+            loader:"less-loader"
+          }
+        ]
+      }
     ]
-  }
+  },
+  plugins:[
+    new HtmlWebpackPlugin({
+      filename: path.resolve(__dirname,'index.html'),
+      template: 'src/index.ejs',
+      inject:false
+    })
+  ],
 };
